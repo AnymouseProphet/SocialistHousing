4747a94665a65b2275fbf2ccdb7cd3886408c3f2
AnymouseProphet <anymouseprophet@protonmail.com>
2021-05-28 07:31:43 -0700

    script cleanup

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # test the LaTeX
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing.tex
+pdflatex SocialistHousing.tex
 if [ $? != 0 ]; then
   echo "LaTeX error, exiting"
   exit 1
 fi
 
 # two more runs
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing.tex
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing.tex
+pdflatex SocialistHousing.tex && pdflatex SocialistHousing.tex
 
 # make non-canonical print version
 sed -e s?"^\\\newcommand{\\\canonicalversion}{yes}"?"\\\newcommand{\\\canonicalversion}{no}"? < SocialistHousing.tex > SocialistHousing-Print.tex
 
 # three runs
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing-Print.tex
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing-Print.tex
-/usr/local/texlive/2021/bin/x86_64-linux/pdflatex SocialistHousing-Print.tex
+pdflatex SocialistHousing-Print.tex && pdflatex SocialistHousing-Print.tex && pdflatex SocialistHousing-Print.tex
 
 # cleanup
 rm -f SocialistHousing-Print.tex
